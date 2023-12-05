fcdf4f71a37e6748f828151ca1b3f40ba3df8f5c
Dan <dan.pfister27@gmail.com>
2023-12-05 23:54:48 +0100

    improved readability

--- day05/day05.py
+++ day05/day05.py
@@ -8,7 +8,7 @@ def map_source_to_destination_single(old_number, mappings):
         if old_number in range(source, end): return destination - source + old_number # i.e. offset + previous value
     return old_number
 
-def map_source_to_destination_block(block, mappings): # TODO
+def map_source_to_destination_block(block, mappings):
     block_start, block_end = block
     for mapping in mappings:
         mapping_destination, mapping_source, mapping_end = mapping
@@ -41,11 +41,11 @@ def parse_mappings(inputdata):
             current_mapping = []
             while (index in range(1, len(inputdata)) and inputdata[index] != ''):
                 numbers = [int(number) for number in re.findall(r'(\d+)', inputdata[index])]
-                numbers = [numbers[0], numbers[1], numbers[1] + numbers[2]]
+                numbers = [numbers[0], numbers[1], numbers[1] + numbers[2]] # parse as (destination, source, end (non-inclusive))
                 current_mapping.append(numbers)
                 index += 1
             mappings.append(sorted(current_mapping, key=lambda x: x[1]))
-    return mappings # parsed as (destination, source, end (non-inclusive))
+    return mappings
 
 def part1and2(inputdata):
     seeds = [int(seed) for seed in re.findall(r'\s(\d+)', inputdata[0])] # parse input
@@ -55,7 +55,7 @@ def part1and2(inputdata):
     for seed in seeds: # iterate over all seeds for part 1
         old_number = seed
         new_number = 0
-        for mapping in mappings:
+        for mapping in mappings: # iterate over all maps
             new_number = map_source_to_destination_single(old_number, mapping)
             old_number = new_number
         if new_number < lowest_location: lowest_location = new_number
@@ -69,10 +69,10 @@ def part1and2(inputdata):
     unprocessed_stack = seeds
     for mapping in mappings:
         next_stack = []
-        while (len(unprocessed_stack) != 0): # loop until all blocks have gone through mapping
+        while (len(unprocessed_stack) != 0): # loop until stack is empty, i.e. all blocks have gone through mapping
             processed_block, unprocessed_block = map_source_to_destination_block(unprocessed_stack.pop(0), mapping)
             next_stack.append(processed_block)
-            if unprocessed_block is not None :
+            if unprocessed_block is not None : # ugly but works
                 if isinstance(unprocessed_block[0], list):
                     unprocessed_stack.extend(unprocessed_block)
                 else:
