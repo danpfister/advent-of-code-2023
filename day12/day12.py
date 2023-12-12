4b69a99f3c12ee6618a6a077196f443a13a551f6
Daniel <dan.pfister27@gmail.com>
2023-12-12 23:35:06 +0100

    improved readability and added comments

--- day12/day12.py
+++ day12/day12.py
@@ -3,7 +3,16 @@ from itertools import product
 
 symbols = ['#', '.']
 
-def check_validity(combination, group):
+def check_validity(combination: str, group: list[int]) -> bool:
+    """returns True if the string matches the grouping
+
+    Args:
+        combination (str): string consisting of # and .
+        group (list[int]): list of integers describing the groups of #'s
+
+    Returns:
+        bool: True if combination matches group, else False
+    """
     hashes = [g for g in combination.split('.') if g]
     if len(hashes) != len(group): return False
     for hash, g in zip(hashes, group):
@@ -16,7 +25,7 @@ def parse_input(inputdata):
     groups = [[int(num) for num in line[1].split(',')] for line in data]
     return springs, groups
 
-def part1and2(inputdata):
+def part1(inputdata):
     springs, groups = parse_input(inputdata)
     valid_combinations = 0
     for spring, group in zip(springs, groups):
@@ -28,10 +37,11 @@ def part1and2(inputdata):
             if check_validity(replaced, group): valid_combinations += 1
     print(f"PART 1: the sum of the possible arrangements of springs is: {valid_combinations}")
         
-
+# brute force approach not feasible for part 2
 def part2(inputdata): pass
 
 if __name__ == '__main__':
-    inputfile = open(r'C:\Users\danpf\Github\advent-of-code-2023\day12\test.txt', 'r')
+    inputfile = open(r'C:\Users\danpf\Github\advent-of-code-2023\day12\input.txt', 'r')
     inputdata = np.asarray([line.strip() for line in inputfile])
-    part1and2(inputdata)
+    part1(inputdata)
+    part2(inputdata)
